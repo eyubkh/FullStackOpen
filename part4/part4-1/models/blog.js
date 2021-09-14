7cb1b38d3e1be2c60c824babbf4fcc1bb267aff6
Eyub <eyub@eyubs-mbp.home>
2021-09-14 19:24:32 +0100

    safety commit

--- part4/part4-1/models/blog.js
+++ part4/part4-1/models/blog.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: {
-    type: String
-  },
-  author: {
-    type: String
-  },
-  url: {
-    type: String
-  },
-  likes: {
-    type: Number
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+    delete returnObject._id
+    delete returnObject.__v
   }
 })
 
