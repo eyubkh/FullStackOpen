b2290f6673ec1bccbc8922d447fa2b4dc1a3f738
Eyub <eyub@eyubs-mbp.home>
2021-09-21 16:00:11 +0100

    part 4 done

---
+++ part4/part4-2/controllers/users.js
@@ -0,0 +1,26 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcryt = require('bcrypt')
+
+usersRouter.get('/', async (request, response) => {
+  const result = await User.find({}).populate('blogs', { author: 1, title: 1 })
+  response.status(200).json(result)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const { body } = request
+  const { username, name, password } = body
+  if (password.length <= 3 || username.length <= 3) {
+    response.status(400).send({ error: 'password or username less than 3 latters' })
+  }
+  try {
+    const passwordHash = await bcryt.hash(password, 10)
+    const newUser = new User({ username, name, passwordHash })
+    const result = await newUser.save()
+    response.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = usersRouter
---
+++ part4/part4-2/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  url: String,
+  title: String,
+  author: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
